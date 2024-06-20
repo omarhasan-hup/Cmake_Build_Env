2c33b842c1ac02e17b8028deed9f98263770dc63
Omar Hasan <engomar582@gmail.com>
2024-06-20 23:48:40 +0300

    adding SWC_three wiht all the required changes for it to build

--- src_code/main.c
+++ src_code/main.c
@@ -1,9 +1,11 @@
 #include <stdio.h>
 #include"SWC_one.h"
 #include"SWC_two.h"
+#include"SWC_three.h"
 void main (void)
 {
     printf("Hello for the first time Cmake \r\n");
     print_func();
     print_func_two();
+    print_func_tree();
 }
