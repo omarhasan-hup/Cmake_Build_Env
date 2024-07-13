7cbd232a2b4f3288c5b091ca6bad90037a8ff076
Omar Hasan <engomar582@gmail.com>
2024-07-13 19:54:21 +0300

    First successful cmake generation for the avr archi

---
+++ cross_compile_avr/avr.cmake
@@ -0,0 +1,8 @@
+set(AVR_TOOLCHAIN_PATH /opt/AVR_Tool_Chain/avr8-gnu-toolchain-linux_x86_64)
+set(CMAKE_C_COMPILER /opt/AVR_Tool_Chain/avr8-gnu-toolchain-linux_x86_64/bin/avr-gcc )
+set(CMAKE_CXX_COMPILER /opt/AVR_Tool_Chain/avr8-gnu-toolchain-linux_x86_64/bin/avr-g++)
+set(CMAKE_SYSTEM_NAME Generic)
+set(CMAKE_SYSTEM_PROCESSOR "avr")
+set(CMAKE_FIND_ROOT_PATH_MODE_PROGRAM NEVER)
+set(CMAKE_FIND_ROOT_PATH_MODE_LIBRARY ONLY)
+set(CMAKE_FIND_ROOT_PATH_MODE_INCLUDE ONLY)
--- src_code/main.c
+++ src_code/main.c
@@ -1,11 +1,35 @@
-#include <stdio.h>
-#include"SWC_one.h"
-#include"SWC_two.h"
-#include"SWC_three.h"
-void main (void)
+// #include <stdio.h>
+// #include"SWC_one.h"
+// #include"SWC_two.h"
+// #include"SWC_three.h"
+// void main (void)
+// {
+//     printf("Hello for the first time Cmake \r\n");
+//     print_func();
+//     print_func_two();
+//     print_func_tree();
+// }
+
+/*****************************************Cross compilation main.c**************************************************************/ 
+
+#include <avr/io.h>
+#include <util/delay.h>
+
+int main(void)
 {
-    printf("Hello for the first time Cmake \r\n");
-    print_func();
-    print_func_two();
-    print_func_tree();
+	DDRC  = 0xFF;	// Configure all PORTC pins as output pins
+	PORTC = 0x01;   // First led is on at the beginning (Positive Logic configuration)
+
+	while(1)
+	{
+
+		_delay_ms(500);
+		PORTC = (PORTC<<1); //every time only one led is ON
+		if(PORTC == 0x00)
+		{
+			/* In case PORTC equals ZERO after 8 shifts
+			 * turn on the first led to start rolling again */
+			PORTC = 0x01;
+		}
+	}
 }
