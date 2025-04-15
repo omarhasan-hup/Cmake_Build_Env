8e9bd446b5e5ffa1016980de44b6d1cdccc942b7
oshawky <omar.shawky@valeo.com>
2025-04-16 01:10:58 +0200

    Add a p2function trial in a native compilation evironemnt
    the pointer to function is intializd in the SWC_One and externed to the main.c

--- src_code/SWC_one/SWC_one.c
+++ src_code/SWC_one/SWC_one.c
@@ -1,5 +1,11 @@
 #include"SWC_one.h"
+extern void (*p2function)(void);
 void print_func(void)
 {
     printf("This is the SWC_one print>>>>>>>>>>> \r\n");
+}
+void nothing(void)
+{
+    p2function = &print_func;
+
 }
--- src_code/main.c
+++ src_code/main.c
@@ -2,10 +2,12 @@
 #include"SWC_one.h"
 #include"SWC_two.h"
 #include"SWC_three.h"
+void (*p2function)(void)=NULL_PTR;
 void main (void)
 {
     printf("Hello for the first time Cmake \r\n");
-    print_func();
+    nothing();
+    (*p2function)();
     print_func_two();
     print_func_tree();
 }
